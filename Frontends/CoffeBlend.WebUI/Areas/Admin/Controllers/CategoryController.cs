3b4e816f6255fa2b608055d043e333738bc1ad60
Sinantosun <sinan_tosun4141@hotmail.com>
2024-09-06 19:44:05 +0300

    --

---
+++ Frontends/CoffeBlend.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using CoffeBlend.DtoLayer.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CoffeBlend.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7245/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsondata);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent strcontent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7245/api/Categories", strcontent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData["Status"] = "Kayıt Eklendi";
+                TempData["Icon"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7245/api/Categories/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultCategoryByIdDto>(jsondata);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent strcontent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7245/api/Categories", strcontent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData["Status"] = "Kayıt Güncellendi";
+                TempData["Icon"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7245/api/Categories/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData["Status"] = "Kayıt Silindi";
+                TempData["Icon"] = "success";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}

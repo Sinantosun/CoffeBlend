20e2c110628321c8e0c7aee5a670a841d170daa3
Sinantosun <sinan_tosun4141@hotmail.com>
2024-09-10 13:11:22 +0300

    ---

---
+++ Frontends/CoffeBlend.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using CoffeBlend.DtoLayer.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CoffeBlend.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ProductController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage2 = await client.GetAsync("https://localhost:7245/api/Products/GetProductListWithCategory");
+			if (responseMessage2.IsSuccessStatusCode)
+			{
+				var jsondata2 = await responseMessage2.Content.ReadAsStringAsync();
+				var values2 = JsonConvert.DeserializeObject<List<ResultPrdouctWithCategory>>(jsondata2);
+				return View(values2);
+			}
+			return View();
+        }
+    }
+}
--- Frontends/CoffeBlend.WebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
+++ Frontends/CoffeBlend.WebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoffeBlend.DtoLayer.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CoffeBlend.WebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultTestimonialComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7245/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsondata);
+                return View(values);
+            }
             return View();
         }
     }
